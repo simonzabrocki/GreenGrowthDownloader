23abba64acfc5d293bb86bc466f4c761afa0b878
simon.zabrocki <simon.zabrocki@gmail.com>
2020-10-08 18:19:24 +0900

    refactor downloader

---
+++ download.py
@@ -0,0 +1,44 @@
+
+import argparse
+import json
+import pandas as pd
+from colorama import Fore, init
+from src.downloaders.downloader import CW_Downloader, SDG_Downloader, WB_Downloader
+
+
+def download(API_params, folder_path, downloader):
+    init(autoreset=True)
+
+    for d in API_params:
+        params = d['params']
+        path = f"{folder_path}/{d['GGI_code']}.json"
+
+        print(f'Fetching from {downloader.API_URL} with parameters: ')
+        [print(f'{key}: {params[key]}') for key in params.keys()]
+
+        try:
+            downloader.download_data(path, params)
+            print(Fore.GREEN + 'Done')
+        except Exception as e:
+            print(Fore.RED + 'Something went wrong: ', e)
+
+def download_script():
+
+    with open('params/APIs/CW.json', 'r') as file:
+        CW_params = json.load(file)
+    with open('params/APIs/SDG.json', 'r') as file:
+        SDG_params = json.load(file)
+    with open('params/APIs/WB.json', 'r') as file:
+        WB_params = json.load(file)
+
+    download(CW_params, 'data/CW_API',
+             CW_Downloader(API_URL='https://www.climatewatchdata.org/api/v1/data/historical_emissions'))
+    download(SDG_params, 'data/SDG_API',
+             SDG_Downloader(API_URL='https://unstats.un.org/SDGAPI/v1/sdg/Series/Data'))
+    download(WB_params, 'data/WB_API',
+             WB_Downloader(API_URL='https://api.worldbank.org/v2/country/all/indicator/'))
+
+
+if __name__ == '__main__':
+
+    download_script()
---
+++ src/downloaders/downloader.py
@@ -0,0 +1,86 @@
+import abc
+import logging
+import requests
+import json
+
+import wbdata
+
+
+class Downloader(metaclass=abc.ABCMeta):
+    def __init__(self, API_URL):
+        self.API_URL = API_URL
+        return None
+
+    @abc.abstractmethod
+    def get_data(self, params):
+        pass
+
+    def download_data(self, path, params):
+
+        data = self.get_data(params)
+
+        with open(path, 'w') as file:
+            json.dump(data, file)
+        return None
+
+
+class CW_Downloader(Downloader):
+
+    def get_data(self, params):
+        first_request = requests.get(self.API_URL, params=params)
+        data = first_request.json()['data']
+
+        if 'next' in first_request.links.keys():
+            has_next = True
+            next_url = first_request.links['next']['url']
+        else:
+            has_next = False
+
+        while has_next:
+            next_request = requests.get(next_url)
+            data += (next_request.json()['data'])
+
+            if 'next' in next_request.links.keys():
+                next_url = next_request.links['next']['url']
+            else:
+                has_next = False
+
+        return data
+
+
+class SDG_Downloader(Downloader):
+
+    def get_data(self, params):
+
+        params['pageSize'] = int(1e9)
+        request = requests.get(self.API_URL, params=params)
+        data = request.json()['data']
+        return data
+
+
+class WB_Downloader(Downloader):
+
+    def get_data(self, params):
+        indicator = params['indicator']
+        url = f'{self.API_URL}/{indicator}'
+        params = {'format': 'json', 'per_page': 1}
+
+        # request to get the number of element and make a full request
+        pre_request = requests.get(url, params=params)
+
+
+        total = pre_request.json()[0]['total']
+        params['per_page'] = total
+
+
+        # actual request
+        logger.debug(f'Full request status code {request.status_code}')
+
+        data = request.json()
+
+        data[0]['Source'] = wbdata.get_indicator(indicator)[0]['sourceOrganization']  # Well maybe we could just use wbdata alltogether :/
+
+        return data
+
+
+
