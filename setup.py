35b22e5ca67be9ad80d438fa969c776f0121a94e
Simon <simon.zabrocki@gmail.com>
2021-06-01 11:15:45 +0900

    update freaking setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ggdata',
-    version='0.1.29',
+    version='0.1.32',
     description='A Python package for downloading data from public APIs',
     url='https://github.com/simonzabrocki/GreenGrowthDownloader',
     author='Simon Zabrocki',
@@ -27,8 +27,7 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     scripts=['ggdownload'],
-    include_package_data=True,
     package_data={
         '': ['params/*.json', 'params/*.csv']
-    }
+    },
 )
